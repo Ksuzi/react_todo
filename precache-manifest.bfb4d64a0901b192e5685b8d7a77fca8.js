4f527a37f3dd03accc3a66d492230be7c05a7142
Ksuzi <kseniaterenko@gmail.com>
2019-08-30 19:14:05 +0300

    Updates

---
+++ precache-manifest.bfb4d64a0901b192e5685b8d7a77fca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c8002084b39b41b09be1fc9ac3626d",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "9dc93435dfc027d1f1a0",
+    "url": "/react_todo/static/css/main.0fbefffc.chunk.css"
+  },
+  {
+    "revision": "b8d6a662e1a5e05fdab2",
+    "url": "/react_todo/static/js/2.0a34d0b2.chunk.js"
+  },
+  {
+    "revision": "9dc93435dfc027d1f1a0",
+    "url": "/react_todo/static/js/main.c5da2723.chunk.js"
+  },
+  {
+    "revision": "481099036e3c800d2593",
+    "url": "/react_todo/static/js/runtime~main.b803c4f6.js"
+  }
+]);
